dd316e4cdcd2efd22c040c2fe61214a89e88a908
Mandla Moyo <mandla@noredink.com>
2024-05-01 23:21:00 -0700

    Added score threshold to vector db retrieval

--- backend/ingest.py
+++ backend/ingest.py
@@ -1,33 +1,42 @@
 from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
 from langchain_community.document_loaders.directory import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.text_splitter import MarkdownTextSplitter
-from langchain_text_splitters import Language
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
 from langchain.embeddings import CacheBackedEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
 from langchain.storage import LocalFileStore
 from langchain.embeddings.ollama import OllamaEmbeddings
 
+import shutil
+
 from settings import settings
 
 
+def clear_vector_store():
+    shutil.rmtree(settings.vector_store_path, ignore_errors=True)
+    shutil.rmtree(settings.cache_folder, ignore_errors=True)
+
+
 def load_docs():
-    loader = DirectoryLoader(settings.download_folder, glob="**/*.md", loader_cls=UnstructuredMarkdownLoader)
+    loader = DirectoryLoader(
+        settings.download_folder, glob="**/*.md", loader_cls=UnstructuredMarkdownLoader
+    )
     return loader.load()
+
 
 def chunk_docs(raw_documents):
     text_splitter = RecursiveCharacterTextSplitter.from_language(
-        Language.MARKDOWN, 
-        chunk_size = settings.chunk_size,
-        chunk_overlap = settings.chunk_overlap,
-        length_function = len,
+        Language.MARKDOWN,
+        chunk_size=settings.chunk_size,
+        chunk_overlap=settings.chunk_overlap,
+        length_function=len,
     )
 
     return text_splitter.split_documents(raw_documents)
 
+
 def get_embeddings_model():
     return OllamaEmbeddings(model=settings.ollama_embeddings_model)
+
 
 def get_embeddings():
     store = LocalFileStore(settings.cache_folder)
@@ -37,19 +46,26 @@ def get_embeddings():
         core_embeddings_model, store, namespace=core_embeddings_model.model
     )
 
+
 def ingest_docs(documents):
     embeddings = get_embeddings()
-    return FAISS.from_documents(documents, embeddings)
+    return FAISS.from_documents(documents, embeddings, normalize_L2=True)
+
 
 def build_vector_store():
+    clear_vector_store()
     raw_documents = load_docs()
     documents = chunk_docs(raw_documents)
     vector_store = ingest_docs(documents)
     vector_store.save_local(settings.vector_store_path)
 
+
 def load_vector_store():
     embeddings = get_embeddings()
-    return FAISS.load_local(settings.vector_store_path, embeddings, allow_dangerous_deserialization=True)
+    return FAISS.load_local(
+        settings.vector_store_path, embeddings, allow_dangerous_deserialization=True
+    )
+
 
 if __name__ == "__main__":
     build_vector_store()
--- backend/retriever.py
+++ backend/retriever.py
@@ -1,58 +1,20 @@
 from langchain.chains import RetrievalQA
 from langchain.callbacks import StdOutCallbackHandler
-from langchain.chains.qa_with_sources.retrieval import RetrievalQAWithSourcesChain
 from langchain_community.llms import Ollama
 from pydantic import BaseModel
 from typing import Dict, List, Optional
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-from langchain_core.prompts import PromptTemplate, MessagesPlaceholder, ChatPromptTemplate
-from langchain_core.runnables import RunnableBranch, RunnableLambda, RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
-from operator import itemgetter
+from langchain_core.runnables import RunnableLambda
 from langchain_core.messages import AIMessage, HumanMessage
 
 from ingest import load_vector_store, get_embeddings_model
 from settings import settings
 
-RESPONSE_TEMPLATE = """\
-You are an expert programmer and problem-solver, tasked with answering any question \
-about Langchain.
-
-Generate a comprehensive and informative answer of 80 words or less for the \
-given question based solely on the provided search results (URL and content). You must \
-only use information from the provided search results. Use an unbiased and \
-journalistic tone. Combine search results together into a coherent answer. Do not \
-repeat text. Cite search results using [${{number}}] notation. Only cite the most \
-relevant results that answer the question accurately. Place these citations at the end \
-of the sentence or paragraph that reference them - do not put them all at the end. If \
-different results refer to different entities within the same name, write separate \
-answers for each entity.
-
-You should use bullet points in your answer for readability. Put citations where they apply
-rather than putting them all at the end.
-
-If there is nothing in the context relevant to the question at hand, just say "Hmm, \
-I'm not sure." Don't try to make up an answer.
-
-Anything between the following `context`  html blocks is retrieved from a knowledge \
-bank, not part of the conversation with the user. 
-
-<context>
-    {context} 
-<context/>
-
-REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm \
-not sure." Don't try to make up an answer. Anything between the preceding 'context' \
-html blocks is retrieved from a knowledge bank, not part of the conversation with the \
-user.\
-"""
-
-
 
 class ChatRequest(BaseModel):
     question: str
     chat_history: Optional[List[Dict[str, str]]]
+
 
 def format_docs(docs) -> str:
     formatted_docs = []
@@ -60,6 +22,7 @@ def format_docs(docs) -> str:
         doc_string = f"<doc id='{i}'>{doc.page_content}</doc>"
         formatted_docs.append(doc_string)
     return "\n".join(formatted_docs)
+
 
 def serialize_history(request):
     chat_history = request.get("chat_history") or []
@@ -78,80 +41,18 @@ vector_store = load_vector_store()
 retriever = vector_store.as_retriever()
 
 
-qa_with_sources_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    retriever=retriever,
-    callbacks=[handler],
-    return_source_documents=True,
-
+answer_chain = RunnableLambda(
+    lambda x: RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=vector_store.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={"score_threshold": x["threshold"]},
+        ),
+        callbacks=[handler],
+        return_source_documents=True,
+    )(x)
 )
 
-# conversation_chain = condense_question_chain | retriever
-# retriever_chain = RunnableBranch(
-#     (
-#         RunnableLambda(lambda x: bool(x.get("chat_history"))).with_config(
-#             run_name="HasChatHistoryCheck"
-#         ),
-#         conversation_chain.with_config(run_name="RetrievalChainWithHistory"),
-#     ),
-#     (
-#         RunnableLambda(itemgetter("question")) | retriever
-#     ).with_config(run_name="RetrievalChainWithNoHistory"),
-# ).with_config(run_name="RouteDependingOnChatHistory")
-
-
-# context = (
-#     RunnablePassthrough.assign(docs=retriever_chain)
-#     .assign(context=lambda x: format_docs(x["docs"]))
-#     .with_config(run_name="RetrieveDocs")
-# )
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", RESPONSE_TEMPLATE),
-#         MessagesPlaceholder(variable_name="chat_history"),
-#         ("human", "{question}"),
-#     ]
-# )
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ('system', "Hello! I'm a language model trained on a large corpus of text. I can answer questions and provide information on a wide range of topics. What would you like to know?"),
-        # MessagesPlaceholder("chat_history"),
-        ("human", "{text}"),
-    ]
-)
-
-answer_chain = qa_with_sources_chain
-
-search_chain = RunnableLambda(lambda x: get_embeddings_model().embed_query(x["query"])) | RunnableLambda(lambda x: vector_store.similarity_search_by_vector(x, k=4))
-# answer_chain = prompt | RunnableLambda(lambda x: qa_with_sources_chain({"query": x["text"]}))
-# answer_chain = prompt | RunnableLambda(itemgetter("text")) | qa_with_sources_chain
-# answer_chain = prompt | context | llm
-
-# answer_chain = RunnablePassthrough.assign(chat_history=serialize_history) | context | default_response_synthesizer
-
-
-
-
-
-# llm = Ollama(model=settings.ollama_llm, temperature=0)
-# handler = StdOutCallbackHandler()
-# vector_store = load_vector_store()
-# retriever = vector_store.as_retriever()
-
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ('system', "Hello! I'm a language model trained on a large corpus of text. I can answer questions and provide information on a wide range of topics. What would you like to know?"),
-#         # MessagesPlaceholder("chat_history"),
-#         ("human", "{text}"),
-#     ]
-# )
-
-# result = RetrievalQA.from_chain_type(
-#     llm=llm,
-#     retriever=retriever,
-#     callbacks=[handler],
-#     return_source_documents=True,
-# )
-
-# answer_chain = prompt | result
+search_chain = RunnableLambda(
+    lambda x: get_embeddings_model().embed_query(x["query"])
+) | RunnableLambda(lambda x: vector_store.similarity_search_by_vector(x, k=4))
