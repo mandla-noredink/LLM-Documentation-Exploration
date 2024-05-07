7a83496c025f9b700424864bbfcb0f5ed0020f5d
Mandla Moyo <mandla@noredink.com>
2024-05-07 15:10:03 -0700

    Implemented pgvector store

--- backend/settings.py
+++ backend/settings.py
@@ -3,6 +3,11 @@ from enum import Enum
 from pydantic_settings import BaseSettings
 
 
+class VectorDB(Enum):
+    LOCAL = "FAISS"
+    REMOTE = "PGVECTOR"
+
+
 class Settings(BaseSettings):
 
     ollama_llm: str = "llama3"
@@ -13,5 +18,16 @@ class Settings(BaseSettings):
     cache_folder: str = ".cache/"
     chunk_size: int = 1000
     chunk_overlap: int = 100
+
+    # For PG Vector Store
+    pg_host: str = "pg_host"
+    pg_port: str = "pg_port"
+    pg_user: str = "pg_user"
+    pg_password: str = "pg_password"
+    pg_dbname: str = "pg_dbname"
+
+    # Vector DB selection
+    vector_db: VectorDB = VectorDB.LOCAL
+
 
 settings = Settings()
